df1b92b1ca1a7ff76b8c2ff37a19dbd308229d41
fullerd3v <fullerd3v@gmail.com>
2022-10-06 17:28:21 +0200

    Corregir errores

--- src/diceroll/Main/FXMLDocumentController.java
+++ src/diceroll/Main/FXMLDocumentController.java
@@ -7,10 +7,11 @@ package diceroll.Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -18,11 +19,60 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    @FXML
+    private ImageView img;
+    
+    Image uno, dos, tres, cuatro, cinco, seis;
+    
+    int opt = 0;
+    
+    @FXML
+    public void imgOnMouseClicked(MouseEvent event){
+        opt = (int)(Math.random() * 7 + 1);
+        System.out.println(opt);
+        switch(opt){
+            case 1:
+                img.setImage(uno);
+                break;
+            case 2:
+                img.setImage(dos);
+                break;
+            case 3:
+                img.setImage(tres);
+                break;
+            case 4:
+                img.setImage(cuatro);
+                break;
+            case 5:
+                img.setImage(cinco);
+                break;
+            case 6:
+                img.setImage(seis);
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
+            
+    @FXML
+    public void imgOnMouseDragged(){
+        //imgOnMouseReleased();
+    }
+            
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        uno = new Image("../Assets/1.png");    
+        dos = new Image("../Assets/2.png");
+        tres = new Image("../Assets/3.png");
+        cuatro = new Image("../Assets/4.png");
+        cinco = new Image("../Assets/5.png");
+        seis = new Image("./Assets/6.png");
+
+        
     }    
     
 }
